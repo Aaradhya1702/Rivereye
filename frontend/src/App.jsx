019fd8ac607f4144eb8d3ab6ee1a265855d1ee49
Aaradhya1702 <jainaaradhya961@gmail.com>
2025-09-20 16:09:52 +0530

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,12 @@ import LandingPage from './pages/Landing';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
+import { I18nextProvider } from "react-i18next";
+import i18n from "./translation";
 
 function App() {
   return (
+    <I18nextProvider i18n={i18n}>
     <div style={{ width: '100vw', minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <Router>
         {/* Navbar visible on all pages */}
@@ -24,6 +27,7 @@ function App() {
         </div>
       </Router>
     </div>
+      </I18nextProvider>
   );
 }
 
---
+++ frontend/src/components/Block/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
+import { withTranslation } from "react-i18next";
+import { Container, TextWrapper, Content } from "./styles";
+
+
+const Block = ({ title, content, t }) => {
+  return (
+    <Container>
+      <h6>{t(title)}</h6>
+      <TextWrapper>
+        <Content>{t(content)}</Content>
+      </TextWrapper>
+    </Container>
+  );
+};
+
+export default withTranslation()(Block);
