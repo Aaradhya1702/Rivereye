cb2560c5bb4b30614184d42f3627b4bf7a3a0984
Aaradhya1702 <jainaaradhya961@gmail.com>
2025-09-20 17:18:26 +0530

    ef

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -6,34 +6,34 @@ const Footer = () => {
     <footer
       style={{
         color: "#101522",
-        backgroundColor: "#fff",
+        backgroundColor: "#e3f2fd", // even lighter blue
         padding: "30px 0",
         textAlign: "center",
       }}
     >
       <div style={{ maxWidth: "1000px", margin: "0 auto" }}>
             <img src={'/logo.png'} alt="Rivereye Logo" style={{ height: '100px', marginRight: '10px' }} />
-        <p style={{ marginBottom: "20px", fontSize: "14px", color: "#101522", }}>
+        <p style={{ marginBottom: "20px", fontSize: "14px",  }}>
           A water quality monitoring platform for the Ganga River.  
           Track pollution levels, get alerts, and access clean data for awareness and research.
         </p>
 
         <div style={{ marginBottom: "20px" }}>
-          <Link to="/" style={{ color: "#101522", margin: "0 10px" }}>
+          <Link to="/" style={{  margin: "0 10px" }}>
             Home
           </Link>
-          <Link to="/dashboard" style={{ color: "#101522", margin: "0 10px" }}>
+          <Link to="/dashboard" style={{  margin: "0 10px" }}>
             Dashboard
           </Link>
-          <Link to="/faq" style={{ color: "#101522", margin: "0 10px" }}>
+          <Link to="/faq" style={{  margin: "0 10px" }}>
             FAQ
           </Link>
-          <Link to="/signup" style={{ color: "#101522", margin: "0 10px" }}>
+          <Link to="/signup" style={{  margin: "0 10px" }}>
             Sign Up
           </Link>
         </div>
 
-        <p style={{ fontSize: "12px", color: "#101522", }}>
+        <p style={{ fontSize: "12px",  }}>
           © {new Date().getFullYear()} Rivereye. Built for monitoring the
           lifeline of India – The Ganga.
         </p>
