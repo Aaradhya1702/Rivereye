b8c5081164fc2e91260e72f81e99a4ee7f5df22f
Aaradhya1702 <jainaaradhya961@gmail.com>
2025-09-21 09:47:09 +0530

    dfe

--- frontend/src/components/InfoSection/InfoSection.jsx
+++ frontend/src/components/InfoSection/InfoSection.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { InfoSec, InfoRow, InfoColumn, TextWrapper, TopLine, Heading, Subtitle, ImgWrapper, Img } from './InfoSection.elements';
 import { Container, Button } from '../../globalStyles';
-import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 const InfoSection = ({
   primary,
